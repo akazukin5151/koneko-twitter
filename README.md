741e6f8246c9be088032e5a4922b510ec97c35db
twenty5151 <tsuiyikching@protonmail.com>
2020-07-25 22:53:36 +0800

    Add -c|--clone option to patcher & develop section to readme

--- README.md
+++ README.md
@@ -11,9 +11,20 @@ A diff file is automatically generated for convenience as [patch.diff](patch.dif
 ```sh
 git clone https://github.com/twenty5151/koneko-twitter.git
 cd koneko-twitter
-./patcher.sh
+./patcher.sh -c  # Short for --clone
 cd build/koneko
 python setup.py develop
 ```
 
 Run with `koneko_twitter`, enter `1` and a twitter username. Pages aren't supported in this example, due to the "infinite" scroll nature of twitter, and the twitter api I'm using.
+
+# Develop
+
+* Edit the files
+* If needed, copy `.py` files from `koneko` to root and edit them
+* Finally run `./patcher.sh`
+    * Without the clone option, the patcher just moves the files
+* Test with `koneko_twitter` (assuming it is already installed with `python setup.py develop`)
+
+Finally, you should run `./patcher.sh -c` for a clean build plus patch. A diff file will also be generated.
+
--- patcher.sh
+++ patcher.sh
@@ -1,19 +1,39 @@
-if [ -d 'build' ]; then rm -Rf 'build'; fi
-mkdir -p build
+clone=0
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -c|--clone) clone=1 ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if [ $clone -eq 1 ]; then
+    if [ -d 'build' ]; then rm -Rf 'build'; fi
+    mkdir -p build
+    cd build
+
+    git clone https://github.com/twenty5151/koneko.git
+
+    cp ../*.py koneko/koneko
+    rm koneko/setup.py
+    mv koneko/koneko/setup.py koneko/
+    cp ../requirements.txt koneko/
+
+    # For reference, doesn't include koneko -> koneko-twitter search and replace
+    cd koneko
+    git diff > ../../patch.diff
+
+    grep -rl 'koneko' . | grep -v '.git' | xargs sed -i 's/koneko/koneko_twitter/g'
+    mv koneko koneko_twitter
+
+    exit 0
+fi
+
+
+echo "Only moving files, no diff generated (use -c|--clone)"
 cd build
-
-git clone https://github.com/twenty5151/koneko.git
-
-cp ../*.py koneko/koneko
+cp ../*.py koneko/koneko_twitter
 rm koneko/setup.py
-mv koneko/koneko/setup.py koneko/
+mv koneko/koneko_twitter/setup.py koneko/
 cp ../requirements.txt koneko/
-
-# For reference, doesn't include koneko -> koneko-twitter search and replace
-cd koneko
-git diff > ../../patch.diff
-
-grep -rl 'koneko' . | grep -v '.git' | xargs sed -i 's/koneko/koneko_twitter/g'
-mv koneko koneko_twitter
-
 
